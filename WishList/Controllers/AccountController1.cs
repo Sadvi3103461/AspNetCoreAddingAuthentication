1d2dbc132d20a30eb581b905ac21d20e818d859d
vmadmin <vmadmin@vssql>
2022-07-15 14:32:42 +0000

    3.1

---
+++ WishList/Controllers/AccountController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
