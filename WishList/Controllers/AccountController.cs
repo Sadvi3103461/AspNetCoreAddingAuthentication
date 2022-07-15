aa9799ace9c9f95c3c5cc741f82902df24c42666
vmadmin <vmadmin@vssql>
2022-07-15 14:55:04 +0000

    3.2

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
     [Authorize]
     public class AccountController : Controller
     {
